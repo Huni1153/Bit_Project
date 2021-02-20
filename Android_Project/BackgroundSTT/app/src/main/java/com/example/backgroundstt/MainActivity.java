1ff22bde5b79bb7bd1a98a0606bd805728d5f749
Huni <zxca1520@gmail.com>
2021-02-21 00:20:49 +0900

    background STTModule complete!

---
+++ Android_Project/BackgroundSTT/app/src/main/java/com/example/backgroundstt/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.backgroundstt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.main_btn_sttStart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(MainActivity.this, Recognition.class));
+            }
+        });
+
+        findViewById(R.id.main_btn_sttEnd).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(MainActivity.this, Recognition.class));
+            }
+        });
+    }
+}
