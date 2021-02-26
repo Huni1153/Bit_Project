96a617992fd678cc75146fb78788d6711d2b49a5
Huni <zxca1520@gmail.com>
2021-02-27 04:12:38 +0900

    chatting ui modify!

--- Android_Project/BackgroundSTT/app/src/main/java/com/example/backgroundstt/ChatActivity.java
+++ Android_Project/BackgroundSTT/app/src/main/java/com/example/backgroundstt/ChatActivity.java
@@ -1,11 +1,15 @@
 package com.example.backgroundstt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.MotionEventCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 
@@ -32,15 +36,49 @@ public class ChatActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(manager); // LayoutManager 등록
         recyclerView.setAdapter(new MyAdapter(dataList));  // Adapter 등록
 
+        recyclerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                finish();
+                return false;
+            }
+        });
+
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+
+        int action = MotionEventCompat.getActionMasked(event);
+
+        switch(action) {
+            case (MotionEvent.ACTION_DOWN) :
+                Log.d("이벤트 확인","Action was DOWN");
+                return true;
+            case (MotionEvent.ACTION_MOVE) :
+                Log.d("이벤트 확인","Action was MOVE");
+                return true;
+            case (MotionEvent.ACTION_UP) :
+                Log.d("이벤트 확인","Action was UP");
+                return true;
+            case (MotionEvent.ACTION_CANCEL) :
+                Log.d("이벤트 확인","Action was CANCEL");
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE) :
+                Log.d("이벤트 확인","Movement occurred outside bounds " +
+                        "of current screen element");
+                return true;
+            default :
+                return super.onTouchEvent(event);
+        }
+    }
 
     public void initializeData()
     {
         dataList = new ArrayList<>();
 
-        dataList.add(new DataItem("안녕하세요",  Code.ViewType.LEFT_CONTENT));
-        dataList.add(new DataItem("안녕하세요",   Code.ViewType.RIGHT_CONTENT));
+        dataList.add(new DataItem("여긴 비서가 음성으로 말하면서 출력될 말풍선 입니다.",  Code.ViewType.LEFT_CONTENT));
+        dataList.add(new DataItem("여긴 우리가 음성으로 말하면 출력될 말풍선 입니다.",   Code.ViewType.RIGHT_CONTENT));
 
     }
 }
